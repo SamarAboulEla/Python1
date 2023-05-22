ae69b45615fece185c21bd74da13b6bd380c0f80
Samar <109899394+SamarAboulEla@users.noreply.github.com>
2023-05-22 20:34:18 +0300

    Create 00 strings.py

---
+++ Tutorial1/00 strings.py
@@ -0,0 +1,10 @@
+# upper & lower to change the string to upper & lower case
+# there is also isupper & islower which return boolean values
+print("Hello\n\t\""+ character_name.upper() + "\"!")
+# len for string length, 0 indexing for strings
+# index returns the index of the first string which matches the input string
+print(len(character_name))
+print(character_name[6])
+print(character_name.index("Ela"))
+# we can replace some part of a string as shown
+print(character_name.replace("a", "A"))
